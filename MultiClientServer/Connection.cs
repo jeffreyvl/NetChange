0311aa00abdfc7f7960a2475d9f3eeb12a5f4900
jeffreyvl <jeffrey.github@outlook.com>
2017-12-13 23:07:38 +0100

    initial commit

---
+++ MultiClientServer/Connection.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace MultiClientServer
+{
+    class Connection
+    {
+        public StreamReader Read;
+        public StreamWriter Write;
+        public int poort;
+        public Dictionary<int, int> RoutingTable = new Dictionary<int, int>();
+        public object rtLocked = new object();
+
+        // Connection heeft 2 constructoren: deze constructor wordt gebruikt als wij CLIENT worden bij een andere SERVER
+        public Connection(int port)
+        {
+            poort = port;
+            TcpClient client = new TcpClient("localhost", port);
+            Read = new StreamReader(client.GetStream());
+            Write = new StreamWriter(client.GetStream());
+            Write.AutoFlush = true;
+
+
+            // De server kan niet zien van welke poort wij client zijn, dit moeten we apart laten weten
+            Write.WriteLine("Poort: " + Program.MijnPoort);
+
+            Console.WriteLine($"Verbonden: {port}");
+            lock(Program.prefNBLocked)
+                Program.SendRoutingTable(this);
+            // Start het reader-loopje
+            new Thread(ReaderThread).Start();
+        }
+
+        // Deze constructor wordt gebruikt als wij SERVER zijn en een CLIENT maakt met ons verbinding
+        public Connection(StreamReader read, StreamWriter write, int port)
+        {
+            Read = read; Write = write; poort = port;
+            Console.WriteLine($"Verbonden: {port}");
+            lock(Program.prefNBLocked)
+                Program.SendRoutingTable(this);
+            // Start het reader-loopje
+            new Thread(ReaderThread).Start();
+        }
+
+        // LET OP: Nadat er verbinding is gelegd, kun je vergeten wie er client/server is (en dat kun je aan het Connection-object dus ook niet zien!)
+
+        // Deze loop leest wat er binnenkomt en print dit
+        public void ReaderThread()
+        {
+            try
+            {
+                while (true)
+                {
+                    string s = Read.ReadLine();
+                    string[] split = s.Split(' ');
+                    if (split[0] == "UpdateRoutingTable")
+                    {
+                        UpdateRoutingTable(int.Parse(s.Split(' ')[1]), int.Parse(s.Split(' ')[2]));
+                    }
+                    else if (split[0] == "Message")
+                    {
+                        var recip = int.Parse(split[1]);
+                        if (recip == Program.MijnPoort)
+                        {
+                            Console.WriteLine(string.Join(" ", split.Skip(2)));
+                        }
+                        else
+                        {
+                            var sendTo = Program.PreferredNeighbour[recip].Item2;
+                            Program.Buren[sendTo].Write.WriteLine(s);
+                            Console.WriteLine($"Bericht voor {recip} doorgestuurd naar {sendTo}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(Read.ReadLine());
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"Verbroken: {poort}");
+                lock (Program.bLocked)
+                    Program.Buren.Remove(poort);
+
+                List<int> nodes = new List<int>();
+                lock (Program.prefNBLocked)
+                    foreach (var node in Program.PreferredNeighbour.Keys)
+                        nodes.Add(node);
+
+                int length = nodes.Count;
+                for (var i = 0; i < length; i++)
+                    Program.UpdatePreferredNeighbour(nodes[i]);
+
+            } // Verbinding is kennelijk verbroken
+        }
+
+        public void Message(int to, string bericht)
+        {
+            Write.WriteLine($"Message {to} {bericht}");
+        }
+
+        public void UpdateRoutingTable(int node, int dis)
+        {
+            if (node != Program.MijnPoort)
+            {
+                lock (rtLocked)
+                {
+                    if (!RoutingTable.ContainsKey(node))
+                        RoutingTable.Add(node, dis);
+                    else
+                        RoutingTable[node] = dis;
+                }
+                Program.UpdatePreferredNeighbour(node);
+            }
+        }
+
+
+    }
+}
---
+++ MultiClientServer/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+using System.Text;
+
+namespace MultiClientServer
+{
+    class Program
+    {
+        static public int MijnPoort;
+
+        static public SortedDictionary<int, Connection> Buren = new SortedDictionary<int, Connection>();
+        static public Dictionary<int, Tuple<int, int>> PreferredNeighbour = new Dictionary<int, Tuple<int, int>>();
+
+        static public object bLocked = new object();
+        static public object prefNBLocked = new object();
+
+        static void Main(string[] args)
+        {
+            string[] split;
+            MijnPoort = int.Parse(args[0]);
+            Server MijnServer = new Server(MijnPoort);
+            PreferredNeighbour.Add(MijnPoort, Tuple.Create(0, 0));
+            for (var i = 1; i < args.Length; i++)
+            {
+                int poort = int.Parse(args[i]);
+                if (poort > MijnPoort)
+                    continue;
+                Connect(poort);
+            }
+
+            Console.Title = "Netchange " + MijnPoort;
+
+            while (true)
+            {
+                string input = Console.ReadLine().Trim();
+                split = input.Split(' ');
+                int poort;
+                switch (split[0])
+                {
+                    case "R":
+                        // Routing table
+                        lock (prefNBLocked)
+                        {
+                            foreach (var node in PreferredNeighbour)
+                            {
+                                var nearest = node.Value.Item2 == 0 ? "local" : node.Value.Item2.ToString();
+                                if (node.Value.Item1 < 20)
+                                    Console.WriteLine(node.Key + " " + node.Value.Item1 + " " + nearest);
+                            }
+                        }
+                        break;
+                    case "B":
+                        // Send message
+
+                        poort = int.Parse(split[1]);
+                        if (!PreferredNeighbour.ContainsKey(poort))
+                            Console.WriteLine("Hier is geen verbinding naar!");
+                        else
+                        {
+                            string bericht = string.Join(" ", split.Skip(2));
+                            Buren[PreferredNeighbour[poort].Item2].Message(poort, bericht);
+                        }
+
+                        break;
+                    case "C":
+                        // Create connection
+                        poort = int.Parse(split[1]);
+                        Connect(poort);
+                        break;
+                    case "D":
+                        // Break connection
+                        var dPoort = int.Parse(split[1]);
+                        lock(bLocked)
+                        {
+                            if (Buren.ContainsKey(dPoort))
+                                Buren[dPoort].Write.Close();
+                            else
+                                Console.WriteLine($"Geen verbinding met {dPoort}");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+        static void Connect(int poort)
+        {
+            bool connected = false;
+            Connection con = null;
+            lock (bLocked)
+            {
+                if (Buren.ContainsKey(poort))
+                    Console.WriteLine("Hier is al verbinding naar!");
+                else
+                {
+                    while (!connected)
+                    {
+                        try
+                        {
+                            con = new Connection(poort);
+                            Buren.Add(poort, con);
+                            connected = true;
+                        }
+                        catch
+                        {
+                            Thread.Sleep(10);
+                        }
+                    }
+                    UpdatePreferredNeighbour(poort);
+                    Console.WriteLine($"Verbonden: {poort}");
+                    lock (prefNBLocked)
+                         SendRoutingTable(con);
+                }
+            }
+        }
+
+        public static void SendRoutingTable()
+        {
+            for (int j = 0; j < PreferredNeighbour.Count; j++)
+            {
+                var k = PreferredNeighbour.ElementAt(j);
+                for (int i = 0; i < Buren.Count; i++)
+                    Buren.ElementAt(i).Value.Write.WriteLine($"UpdateRoutingTable {k.Key} {k.Value.Item1}");
+            }
+        }
+
+        public static void SendRoutingTable(Connection b)
+        {
+            for (int j = 0; j < PreferredNeighbour.Count; j++)
+            {
+                var k = PreferredNeighbour.ElementAt(j);
+                b.Write.WriteLine($"UpdateRoutingTable {k.Key} {k.Value.Item1}");
+            }
+        }
+
+        public static void SendRoutingTable(int node)
+        {
+            var k = PreferredNeighbour[node];
+            for (int i = 0; i < Buren.Count; i++)
+                Buren.ElementAt(i).Value.Write.WriteLine($"UpdateRoutingTable {node} {k.Item1}");
+
+        }
+
+        public static void SendRoutingTable(int node, int dis)
+        {
+            for (int i = 0; i < Buren.Count; i++)
+                Buren.ElementAt(i).Value.Write.WriteLine($"UpdateRoutingTable {node} {dis}");
+        }
+
+        public static void UpdatePreferredNeighbour(int node)
+        {
+            if (node == MijnPoort)
+                return;
+            int minDis = 100;
+            int prefNB = 0;
+            lock (bLocked)
+            {
+                foreach (var b in Buren)
+                    lock (b.Value.rtLocked)
+                        if (b.Value.RoutingTable.ContainsKey(node) && b.Value.RoutingTable[node] < minDis)
+                        {
+                            minDis = b.Value.RoutingTable[node];
+                            prefNB = b.Key;
+                        }
+
+                var tup = Tuple.Create(minDis + 1, prefNB);
+                lock (prefNBLocked)
+                {
+                    if (minDis < PreferredNeighbour.Count)
+                    {
+                        if (!PreferredNeighbour.ContainsKey(node))
+                        {
+                            PreferredNeighbour.Add(node, tup);
+                            Console.WriteLine($"Afstand naar {node} is nu {minDis + 1} via {prefNB}");
+                            SendRoutingTable(node);
+                        }
+                        else if (!tup.Equals(PreferredNeighbour[node]))
+                        {
+                            PreferredNeighbour[node] = tup;
+                            Console.WriteLine($"Afstand naar {node} is nu {minDis + 1} via {prefNB}");
+                            SendRoutingTable(node);
+                        }
+                    }
+                    else
+                    {
+                        if (PreferredNeighbour.ContainsKey(node))
+                        {
+                            SendRoutingTable(node, minDis + 1);
+                            PreferredNeighbour.Remove(node);
+                        }
+                        Console.WriteLine($"Onbereikbaar: {node}");
+                    }
+                }
+            }
+        }
+    }
+}
+
+
